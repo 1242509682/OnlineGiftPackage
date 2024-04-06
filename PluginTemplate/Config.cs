8305cf85e081507aef13a6074155ed9d7dd364e3
羽学 <143100151+1242509682@users.noreply.github.com>
2024-04-07 02:33:10 +0800

    提交v1.0.9修复了不送物品的问题，移除了广播在线时长的方法，配置文件加入了大量预设物品方案，修复了/在线礼包指令不显示概率的问题，不再根据序列数量来发放物品而是发过一次自动重置为0+1从第一个序列开始发，解决了初始化配置文件时自动序列化问题，加了个计算总概率的新方法。

--- PluginTemplate/Config.cs
+++ PluginTemplate/Config.cs
@@ -67,26 +67,32 @@ namespace 在线礼包
         public string 物品名称 { get; set; }
         [JsonProperty("物品ID")]
         public int 物品ID { get; set; }
-        [JsonProperty("物品数量")]
-        public int[] 物品数量 { get; set; }
         [JsonProperty("所占概率")]
         public int 所占概率 { get; set; }
+        [JsonProperty("物品数量")]
+        public int[] 物品数量 { get; set; }
+
     }
+
 
     public class Config
     {
         public bool 启用 { get; set; } = true;
         [JsonProperty("总概率")]
         public int 总概率 = 100;
-        public TimeSpan 触发时间 { get; set; } = TimeSpan.FromMinutes(30);
-        // 新增广播间隔时间属性（单位：分钟）
-        [JsonProperty("广播间隔时间/分钟")]
-        public int 广播间隔时间 { get; set; } = 30; // 默认间隔30分钟
-        public string 广播消息 { get; set; } = "你已连续在线[c/55CDFF:{0}]分钟";
+        public TimeSpan 触发时间 { get; set; } = TimeSpan.FromSeconds(1800);
         public Dictionary<int, string> 触发序列 { get; set; } = new Dictionary<int, string>();
         public List<Gift> 礼包列表 { get; set; } = new List<Gift>();
-
+        // 添加一个计算总概率的方法
+        public int CalculateTotalProbability() => 礼包列表.Sum(gift => gift.所占概率);
         public static string path = Path.Combine(TShock.SavePath, "在线礼包.json");
+        public Config()
+        {
+            foreach (Gift gift in 礼包列表)
+            {
+                总概率 += gift.所占概率;
+            }
+        }
 
         // 保存配置文件的方法
         public static void SaveConfig(Config config)
@@ -95,63 +101,496 @@ namespace 在线礼包
             File.WriteAllText(Config.path, json);
         }
 
-        private Config()
+        public static Config LoadOrCreateConfig()
         {
-            this.触发时间 = TimeSpan.FromMinutes(30);
-        }
-        public static Config LoadConfig
-        {
-            get
+            Config config = new Config
             {
-                if (!File.Exists(path))
-                {
-                    Config config = new Config();
-                    Gift item1 = new Gift()
-                    {
-                        物品名称 = "铂金币",
-                        物品ID = 74,
-                        物品数量 = new int[] { 3, 8 },
-                        所占概率 = 3,
-                    };
-                    config.礼包列表.Add(item1);
-                    Gift item2 = new Gift()
-                    {
-                        物品名称 = "蠕虫罐头",
-                        物品ID = 4345,
-                        物品数量 = new int[] { 3, 8 },
-                        所占概率 = 3,
-                    };
-                    config.礼包列表.Add(item2);
-                    Gift item3 = new Gift()
-                    {
-                        物品名称 = "草药袋",
-                        物品ID = 3093,
-                        物品数量 = new int[] { 3, 8 },
-                        所占概率 = 3,
-                    };
-                    for (int i = 1; i <= 50; i++)
-                    {
-                        config.触发序列.Add(i * 1, $"你已获得{i}个礼包");
-                    }
-                    File.WriteAllText(path, JsonConvert.SerializeObject(config));
-                    return config;
-                }
-                else
-                {
-                    Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
-                    config.总概率 = 100;
-                    foreach (Gift gift in config.礼包列表)
-                    {
-                        config.总概率 += gift.所占概率;
-                    }
-                    if (config == null)
-                    {
-                        throw new FormatException("配置文件'在线礼包.json'读取出错！");
-                    }
-                    return config;
-                }
+                礼包列表 = new List<Gift>
+        {
+            new Gift
+            {
+                物品名称 = "铂金币",
+                物品ID = 74,
+                所占概率 = 1,
+                物品数量 = new int[] { 2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "蠕虫罐头",
+                物品ID = 4345,
+                所占概率 = 1,
+                物品数量 = new int[] { 2, 5 },
 
+            },
+            new Gift
+            {
+                物品名称 = "草药袋",
+                物品ID = 3093,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "培根",
+                物品ID = 3532,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "木匣",
+                物品ID = 2334,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "铁匣",
+                物品ID = 2335,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+
+            },
+            new Gift
+            {
+                物品名称 = "金匣",
+                物品ID = 2336,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "地牢匣",
+                物品ID = 3205,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "天空匣",
+                物品ID = 3206,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "冰冻匣",
+                物品ID = 4405,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "绿洲匣",
+                物品ID = 4407,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "黑曜石匣",
+                物品ID = 4877,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "海洋匣",
+                物品ID = 5002,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "暴怒药水",
+                物品ID = 2347,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "怒气药水",
+                物品ID = 2349,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "传送药水",
+                物品ID = 2351,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "生命力药水",
+                物品ID = 2345,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "耐力药水",
+                物品ID = 2346,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "强效幸运药水",
+                物品ID = 4479,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "黑曜石皮药水",
+                物品ID = 288,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "羽落药水",
+                物品ID = 295,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "洞穴探险药水",
+                物品ID = 296,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "战斗药水",
+                物品ID = 300,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "挖矿药水",
+                物品ID = 2322,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "生命水晶",
+                物品ID = 29,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "魔镜",
+                物品ID = 50,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "飞虫剑",
+                物品ID = 5129,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "草药袋",
+                物品ID = 3093,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "血泪",
+                物品ID = 4271,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 2 },
+            },
+            new Gift
+            {
+                物品名称 = "幸运马掌",
+                物品ID = 158,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "超亮头盔",
+                物品ID = 4008,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "非负重石",
+                物品ID = 5391,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "水蜡烛",
+                物品ID = 148,
+                所占概率 = 1,
+                物品数量 = new int[] {3, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "暗影蜡烛",
+                物品ID = 5322,
+                所占概率 = 1,
+                物品数量 = new int[] {3, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "肥料",
+                物品ID = 602,
+                所占概率 = 1,
+                物品数量 = new int[] {3, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "魔法灯笼",
+                物品ID = 3043,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "挖矿衣",
+                物品ID = 410,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "挖矿裤",
+                物品ID = 411,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "熔线钓钩",
+                物品ID = 2422,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "无底微光桶",
+                物品ID = 5364,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "无底蜂蜜桶",
+                物品ID = 5302,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "无底熔岩桶",
+                物品ID = 4820,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "无底水桶",
+                物品ID = 3031,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "湿炸弹",
+                物品ID = 4824,
+                所占概率 = 1,
+                物品数量 = new int[] {3, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "恶魔海螺",
+                物品ID = 4819,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "魔法海螺",
+                物品ID = 4263,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "鱼饵桶",
+                物品ID = 4608,
+                所占概率 = 1,
+                物品数量 = new int[] {5, 9 },
+            },
+            new Gift
+            {
+                物品名称 = "花园侏儒",
+                物品ID = 4609,
+                所占概率 = 1,
+                物品数量 = new int[] {2, 5 },
+            },
+            new Gift
+            {
+                物品名称 = "掘墓者铲子",
+                物品ID = 4711,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "万能晶塔",
+                物品ID = 4951,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "月亮领主腿",
+                物品ID = 5001,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "火把神的恩宠",
+                物品ID = 5043,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "工匠面包",
+                物品ID = 5326,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "闭合的虚空袋",
+                物品ID = 5325,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "先进战斗技术：卷二",
+                物品ID = 5336,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "先进战斗技术",
+                物品ID = 4382,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "闪电胡萝卜",
+                物品ID = 4777,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "熔火护身符",
+                物品ID = 4038,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "泰拉魔刃",
+                物品ID = 4144,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+            new Gift
+            {
+                物品名称 = "真空刃",
+                物品ID = 3368,
+                所占概率 = 1,
+                物品数量 = new int[] {1, 1 },
+            },
+        },
+            };
+
+            // 如果需要将总概率限制为100，可以在加载完所有Gift后，根据实际情况调整所占概率
+            foreach (Gift gift in config.礼包列表)
+            {
+                //强制所有Gift的所占概率之和为100，则可以做如下操作：
+                 gift.所占概率 *= 100 / config.礼包列表.Sum(g => g.所占概率);
             }
+
+            // 初始化触发序列，这里只有一条记录，可直接插入无需循环
+            for (int i = 1; i <= 5; i++)
+            {
+                config.触发序列.Add(i * 1, $"你已获得{i}个礼包");
+            }
+
+
+            if (!File.Exists(path))
+            {
+                int totalProbability = config.CalculateTotalProbability();
+                Console.WriteLine($"所有礼包的总概率为：{totalProbability}");
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented, new GiftConverter()));
+            }
+
+
+            // 序列化并写入文件时，使用触发时间的字符串形式
+            File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented, new GiftConverter()));
+            return config;
+        }
+
+
+        public static Gift SelectRandomGift(Config config)
+        {
+            Random random = new Random();
+            int randomNum = random.Next(0, config.CalculateTotalProbability());
+
+            Gift selectedGift = default!;
+            foreach (Gift gift in config.礼包列表)
+            {
+                randomNum -= gift.所占概率;
+                if (randomNum < 0)
+                {
+                    selectedGift = gift;
+                    break;
+                }
+            }
+
+            // 确保找到一个礼包（即使概率设置不正确），在这种情况下，至少返回列表中的第一个礼包
+            return selectedGift ?? config.礼包列表[0];
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Config config = LoadOrCreateConfig();
+
+            // 序列化配置并同时写入文件
+            SaveConfig(config);
+
+            //发放一个随机礼包
+            Gift randomGift = SelectRandomGift(config);
+            Console.WriteLine($"发放的礼包为：{randomGift.物品名称}");
 
         }
     }
--- PluginTemplate/在线礼包.cs
+++ PluginTemplate/在线礼包.cs
@@ -14,59 +14,43 @@ namespace 在线礼包
     {
         // 插件作者信息
         public override string Author => "星夜神花 羽学适配";
-
         // 插件描述
         public override string Description => "在线礼包插件 ";
-
         // 插件名称
         public override string Name => "在线礼包";
-
         // 插件版本号
-        public override Version Version => new Version(1, 0, 0, 8);
-
+        public override Version Version => new Version(1, 0, 0, 9);
+        // 当前系统日期的天数
+        public static int Day = DateTime.Now.Day;
+        // 玩家在线时长字典，用于记录玩家在线时长（按天）
+        readonly Dictionary<string, int> players = new Dictionary<string, int>();
+        // 定时器实例，用于定期检查在线玩家并发放礼包
+        Timer timer;
+        // 配置文件加载实例
+        Config config;
         // 构造函数，初始化插件与游戏关联
         public 在线礼包(Main game) : base(game)
         {
         }
-
-        // 定时器实例，用于定期检查在线玩家并发放礼包
-        Timer timer;
-
-        // 配置文件加载实例
-        Config config;
-
         // 插件初始化方法
         public override void Initialize()
         {
-            // 加载配置文件
-            config = Config.LoadConfig;
-
+            // 加载或创建配置文件
+            config = Config.LoadOrCreateConfig();
             // 注册命令，用于显示礼包获取概率
             Commands.ChatCommands.Add(new Command(GetProbability, "在线礼包"));
-
             // 监听服务器重载事件，以便在重载后重新设置定时器
             TShockAPI.Hooks.GeneralHooks.ReloadEvent += GeneralHooks_ReloadEvent;
-
-            // 初始化定时器，根据配置的触发时间执行Timer_Elapsed方法
-            timer = new Timer(Timer_Elapsed, null, Timeout.Infinite, (int)config.触发时间.TotalMilliseconds);
-            timer.Change(0, (int)config.触发时间.TotalMilliseconds);
+            timer = new Timer(Timer_Elapsed, "", (int)config.触发时间.TotalMilliseconds, (int)config.触发时间.TotalMilliseconds);
         }
-
         // 重载事件处理程序
         private void GeneralHooks_ReloadEvent(ReloadEventArgs e)
         {
             // 重载配置文件
-            config = Config.LoadConfig;
-
+            config = Config.LoadOrCreateConfig();
             // 更新定时器触发间隔
             timer.Change(config.触发时间, config.触发时间);
         }
-
-        // 玩家在线时长字典，用于记录玩家在线时长（按天）
-        readonly Dictionary<string, int> players = new Dictionary<string, int>();
-
-        // 当前系统日期的天数
-        public static int Day = DateTime.Now.Day;
 
         // 定时器回调方法，负责发放在线礼包
         private void Timer_Elapsed(object state)
@@ -92,44 +76,45 @@ namespace 在线礼包
                 {
                     continue;
                 }
-
-                // 跳过生命值大于等于2000的玩家
-                if (player.TPlayer.statLifeMax >= 2000)
-                {
-                    continue;
-                }
-
-                // 记录或增加玩家在线时长
-                if (!players.ContainsKey(player.Name))
-                {
-                    players[player.Name] = 0;
-                }
-                else
-                {
-                    players[player.Name] += 1;
-                }
-
-                // 根据玩家在线时长发放对应礼包
-                if (config.触发序列.ContainsKey(players[player.Name]))
-                {
-                    Gift gift = RandGift();
-                    if (gift == null)
+                if (player.Active && player.IsLoggedIn)
+                {   // 跳过生命值大于等于2000的玩家
+                    if (player.TPlayer.statLifeMax >= 2000)
                     {
-                        return;
+                        continue;
                     }
 
-                    // 获取随机物品数量
-                    int itemCount = new Random().Next(minValue: gift.物品数量[0], gift.物品数量[1]);
+                    // 记录或增加玩家在线时长
+                    if (players.ContainsKey(player.Name) == false)
+                    {
+                        players[player.Name] = 0;
+                    }
 
-                    // 给玩家发放物品
-                    player.GiveItem(gift.物品ID, itemCount);
+                    else
+                    {
+                        players[player.Name] += 1;
+                    }
 
-                    // 发送提示消息给玩家
-                    player.SendMessage(string.Format(config.触发序列[players[player.Name]] + " [c/55CDFF:服主]送了个在线礼包 [i/s{2}:{1}]", players[player.Name], gift.物品ID, itemCount), Color.GreenYellow);
-                }
-                if (players[player.Name] % config.广播间隔时间 == 0)
-                {
-                    player.SendMessage(string.Format(config.广播消息, players[player.Name]), Color.GreenYellow);
+                    // 根据玩家在线时长发放对应礼包
+                    if (config.触发序列.ContainsKey(players[player.Name]))
+                    {
+                        Gift gift = RandGift();
+                        if (gift == null)
+                        {
+                            return;
+                        }
+
+                        // 获取随机物品数量
+                        int itemCount = new Random().Next(minValue: gift.物品数量[0], gift.物品数量[1]);
+
+                        // 给玩家发放物品
+                        player.GiveItem(gift.物品ID, itemCount);
+
+                        // 发送提示消息给玩家
+                        player.SendMessage(string.Format(config.触发序列[players[player.Name]] + " [c/55CDFF:服主]送了个在线礼包 [i/s{2}:{1}]", players[player.Name], gift.物品ID, itemCount), Color.GreenYellow);
+
+                        // 如果发放了礼包，重置该玩家的在线时长，以便下一次满足条件时发放下一个礼包
+                        players[player.Name] = 0;
+                    }
                 }
             }
         }
@@ -149,8 +134,7 @@ namespace 在线礼包
                     for (int j = 0; j < 10 && i + j < config.礼包列表.Count; j++)
                     {
                         Gift gift = config.礼包列表[i + j];
-                        double probability = (double)gift.所占概率 / config.总概率;
-                        sb.Append($"[i/s1:{gift.物品ID}]:{probability:0.##}% ");
+                        sb.Append("[i/s1:{0}]:{1:0.##}% ".SFormat(gift.物品ID, 100.0 * ((double)gift.所占概率 / config.总概率)));
                     }
 
                     // 发送给玩家
@@ -160,15 +144,14 @@ namespace 在线礼包
         }
 
         // 随机选取礼包的方法
-        readonly Random rand = new Random();
+        Random rand = new Random();
         public Gift? RandGift()
         {
-            // 随机生成一个索引值
             int index = rand.Next(config.总概率);
             int sum = 0;
 
-            // 根据概率分布随机选择一个礼包
-            for (int i = 1; i < config.礼包列表.Count; i++)
+            // 从索引0开始遍历，修正for循环起点
+            for (int i = 0; i < config.礼包列表.Count; i++)
             {
                 sum += config.礼包列表[i].所占概率;
                 if (index < sum)
@@ -176,8 +159,6 @@ namespace 在线礼包
                     return config.礼包列表[i];
                 }
             }
-
-            // 若未找到符合条件的礼包，则返回null
             return null;
         }
     }
